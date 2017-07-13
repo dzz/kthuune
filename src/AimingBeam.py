47d6a9101ea9f70400a2d16d20bdc4fc4ebe89d8
dzz <devon.zachary@gmail.com>
2017-07-12 22:54:25 -0700

    aim fire mechanic

--- src/AimingBeam.py
+++ src/AimingBeam.py
@@ -11,16 +11,36 @@ class AimingBeam(Object):
         self.light_type = Object.LightTypes.DYNAMIC_TEXTURE_OVERLAY
         self.visible = False
         self.buftarget = "popup"
+        self.fired = False
 
+
+    def fireRanged(self):
+        print("KABOOM!")
+        pass
 
     def tick(self):
         offs = 30
 
         pad = self.floor.player.controllers.get_virtualized_pad(self.floor.player.num)
+
+        if(pad.triggers[0]+1.0<0.1) and self.fired:
+            self.fired = False
+
+        if(pad.triggers[1]>0.7) and (pad.triggers[0]+1.0>0.1):
+            if not self.fired:
+                self.fireRanged()
+                self.fired = True
+
+
         self.size[1] = (pad.triggers[0]+1.0)*4.0;
 
         if(self.size[1] < 0.1):
             self.size[1] = 0.0
+
+        if(self.fired):
+            self.size[1] = 0.0
+
+        print("fired?", self.fired)
 
         offsx = cos(self.floor.player.rad) * offs
         offsy = sin(self.floor.player.rad) * offs
