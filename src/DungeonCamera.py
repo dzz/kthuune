65a37628e207fa52ca787624465d2b0d54e6aae0
dzz <devon.zachary@gmail.com>
2017-09-20 02:01:38 -0700

    more camera nonsense

--- src/DungeonCamera.py
+++ src/DungeonCamera.py
@@ -47,8 +47,8 @@ class DungeonCamera (Camera):
 
             pad = self.player.controllers.get_virtualized_pad( self.player.num )
 
-            aim_offset_x = pad.right_stick[0]*4
-            aim_offset_y = pad.right_stick[1]*4
+            aim_offset_x = pad.right_stick[0]*6
+            aim_offset_y = pad.right_stick[1]*6
 
             self.p[0] = (self.player.p[0]+aim_offset_x)* rate + ((1.0-rate)*self.p[0])
             self.p[1] = (self.player.p[1]+aim_offset_y)* rate + ((1.0-rate)*self.p[1])
