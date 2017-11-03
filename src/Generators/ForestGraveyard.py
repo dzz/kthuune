901f218b0a28e8683ac7b8e918acbdb92135420e
dzz <devon.zachary@gmail.com>
2017-11-02 20:17:43 -0700

    software pickups

--- src/Generators/ForestGraveyard.py
+++ src/Generators/ForestGraveyard.py
@@ -103,7 +103,7 @@ class Crystal(Object):
         self.size = [ sz*0.7,sz*0.7 ]
         self.physics = { "radius" : sz/3, "mass"   : 900000, "friction" : 0.3 }
         self.hitFr = 0
-        self.hp = choice([2,3])
+        self.hp = choice([1,1,2])
 
     def tick(self):
 
@@ -310,6 +310,49 @@ class Blood(Object):
         self.texture = choice(Blood.texture)
         pass
 
+class SoftwarePickup(Object):
+    textures = [ 
+        BGL.assets.get("KT-player/texture/software"),
+    ]
+   
+    def parse(od,df):
+        o = SoftwarePickup( p = [ od["x"], od["y"] ] )
+        return o
+        
+    def customize(self):
+        self.fridx = 0
+        self.texture = SoftwarePickup.textures[0]
+        self.buftarget = "popup"
+        self.base_p = list(self.p)
+        self.tick_type = Object.TickTypes.PURGING
+        self.light_type = Object.LightTypes.DYNAMIC_SHADOWCASTER
+        self.light_color = [ 0.0, 1.0, 1.0, 1.0 ]
+        self.light_radius = 25.
+        self.visible = True
+        self.size = [ 2.5,2.5 ]
+
+    def tick(self):
+        self.visible = True
+
+        self.fridx = (self.fridx + 1) % 80
+ 
+        y_offs = sin(self.fridx*(3.14/40.0))
+        self.light_radius = 25. + y_offs
+
+        self.size[0] = 2.5*y_offs
+        self.p[1] = self.base_p[1] + (y_offs*0.15)
+
+        dx = self.p[0] - self.floor.player.p[0]
+        dy = self.p[1] - self.floor.player.p[1]
+
+        md = (dx*dx) + (dy*dy)
+
+        if (md<1.6):
+            self.floor.objects.remove(self)
+            KSounds.play( KSounds.pickup )
+            return False
+        return True
+
 class ResourcePickup(Object):
     textures = [ 
         BGL.assets.get("KT-player/texture/thorium"),
@@ -324,9 +367,9 @@ class ResourcePickup(Object):
         self.buftarget = "popup"
         self.base_p = list(self.p)
         self.tick_type = Object.TickTypes.PURGING
-        self.light_type = Object.LightTypes.DYNAMIC_SHADOWCASTER
+        self.light_type = Object.LightTypes.NONE
         self.light_color = [ 1.0, 1.0, 1.0, 1.0 ]
-        self.light_radius = 25.
+        self.light_radius = 1.0
         self.trigger_timer = 0
         self.visible = True
 
@@ -2338,6 +2381,9 @@ class ForestGraveyard():
             
             if od["key"] in ["elder" ]:
                 self.objects.append(Elder.parse(od,df))
+
+            if od["key"] in ["software" ]:
+                self.objects.append(SoftwarePickup.parse(od,df))
 
             if od["key"] in ["firepot" ]:
                 self.objects.append(Firepot.parse(od,df))
