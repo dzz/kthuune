dc75f51e4a11ac5d23a75f1c338beb0dd5f63e1a
dzz <devon.zachary@gmail.com>
2017-11-05 20:25:16 -0800

    remove del which causes excepy on exit

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from .src import game_instance as game
+from .src.Game import Game
+
+class glb:
+    tick = 0
+    game = Game()
+    ini = None
 
 def init():
-    game.init()
+    glb.game.init()
 
 def tick():
-    game.tick()
+    glb.tick+=1
+    if(glb.tick>630):
+        glb.tick = 0
+        del glb.game
+        glb.game = Game()
+        glb.game.configure( glb.ini )
+    else:
+        glb.game.tick()
 
 def render():
-    game.render()
+    glb.game.render()
 
 def finalize():
-    print("FINALIZING")
-    game.finalize()
-    del game
-    gc.collect()
+    glb.game.finalize()
 
 def configure( application_ini ):
-    game.configure( application_ini )
+    glb.ini = application_ini
+    glb.game.configure( application_ini )
