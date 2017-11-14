fdff0b0b7cfbfd24ed209ac62823312b98a782d9
dzz <devon.zachary@gmail.com>
2017-11-13 17:32:58 -0800

    sword pickup

--- src/Generators/ForestGraveyard.py
+++ src/Generators/ForestGraveyard.py
@@ -558,6 +558,52 @@ class SoftwarePickup(Object):
             return False
         return True
 
+class SwordPickup(Object):
+    textures = [ 
+        BGL.assets.get("KT-player/texture/sword"),
+    ]
+   
+    def parse(od,df):
+        o = SwordPickup( p = [ od["x"], od["y"] ] )
+        return o
+        
+    def customize(self):
+        self.fridx = 0
+        self.texture = SwordPickup.textures[0]
+        self.buftarget = "popup"
+        self.base_p = list(self.p)
+        self.tick_type = Object.TickTypes.PURGING
+        self.light_type = Object.LightTypes.DYNAMIC_SHADOWCASTER
+        self.light_color = [ 0.0, 1.0, 1.0, 1.0 ]
+        self.light_radius = 25.
+        self.visible = True
+        self.size = [ 2.5,2.5 ]
+        self.rad = 3.14/2
+
+    def tick(self):
+        self.visible = True
+
+        self.fridx = (self.fridx + 1) % 80
+ 
+        y_offs = sin(self.fridx*(3.14/40.0))
+        self.light_radius = 25. + y_offs
+
+        self.size[1] = 2.5*y_offs
+        self.p[1] = self.base_p[1] + (y_offs*0.15)
+
+        dx = self.p[0] - self.floor.player.p[0]
+        dy = self.p[1] - self.floor.player.p[1]
+
+        md = (dx*dx) + (dy*dy)
+
+        if (md<1.6):
+            self.floor.objects.remove(self)
+            KSounds.play( KSounds.pickup )
+            self.floor.player.add_dm_message("You found a SWORD")
+            Abilities.Sword = True
+            return False
+        return True
+
 class ResourcePickup(Object):
     textures = [ 
         BGL.assets.get("KT-player/texture/thorium"),
@@ -2678,6 +2724,10 @@ class ForestGraveyard():
                         self.objects.append(SoftwarePickup.parse(od,df))
                 else:
                     self.objects.append(SoftwarePickup.parse(od,df))
+
+            if od["key"] in ["sword_pickup" ]:
+                if not Abilities.Sword:
+                    self.objects.append(SwordPickup.parse(od,df))
 
             if od["key"] in ["firepot", "firepot_right" ]:
                 self.objects.append(Firepot.parse(od,df))
