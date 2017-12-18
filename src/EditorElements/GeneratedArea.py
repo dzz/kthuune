79dd7db85fdac6a39ceaf97cb5691769802c3f71
dzz <devon.zachary@gmail.com>
2017-12-17 17:43:01 -0800

    shadow tiles

--- src/EditorElements/GeneratedArea.py
+++ src/EditorElements/GeneratedArea.py
@@ -1,8 +1,11 @@
+from random import choice
+
 class GeneratedArea:
     width = 0
     height = 0
     output_data = ""
     tiles = {}
+    shadow_tiles = [ 59,60 ]
 
     def reset():
         GeneratedArea.output_data = ""
@@ -30,8 +33,12 @@ class GeneratedArea:
                     GeneratedArea.add_line("{0}".format(x))
                     GeneratedArea.add_line("{0}".format(y))
                     GeneratedArea.add_line("{0}".format(GeneratedArea.tiles[(x,y)]))
-                #else:
-                #    GeneratedArea.add_line("{0}".format(0))
+                else:
+                    if (x,y-1) in GeneratedArea.tiles:
+                        GeneratedArea.add_line("TILE")
+                        GeneratedArea.add_line("{0}".format(x))
+                        GeneratedArea.add_line("{0}".format(y))
+                        GeneratedArea.add_line("{0}".format(choice( GeneratedArea.shadow_tiles)))
 
     def serialize_player():
         GeneratedArea.add_line("OBJECT")
