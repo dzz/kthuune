849391def9395236e92b16a1e121b837d6abf1a7
dzz <devon.zachary@gmail.com>
2018-04-08 21:09:17 -0700

    this looks like a sub menu

--- resources/player/menu/shader/worldbubble_v.glsl
+++ resources/player/menu/shader/worldbubble_v.glsl
@@ -6,6 +6,7 @@
 uniform vec2 view;
 uniform vec2 scale_local;
 uniform vec2 scale_world;
+uniform float scale_x;
 uniform vec2 translation_local;
 uniform vec2 translation_world;
 uniform float rotation_local;
@@ -17,11 +18,12 @@ out vec2 uv;
 
 void main(void) {
     
-
-
-    gl_Position.xy = (input_position.xy*vec2(0.5,0.66)) + vec2( 0.5,0.0);
+    
+    gl_Position.xy = (input_position.xy*vec2(0.5*scale_x,0.66)) + vec2( 0.5,0.0);
     gl_Position.zw = vec2(0,1);
     uv=uv_position;
+    uv.y = 1.0 - uv.y;
+    //gl_Position.x *= scale_x;
         
 }
 
--- src/Menu/BubbleWorldRenderer.py
+++ src/Menu/BubbleWorldRenderer.py
@@ -1,12 +1,15 @@
 from Beagle import API as BGL
+from math import sin
 
 class BubbleWorldRenderer():
     primitive = BGL.primitive.unit_uv_square
     textures = BGL.assets.get('KT-player/animation/worldbubble')
+    lock_texture = BGL.assets.get('KT-player/texture/lock')
     shader = BGL.assets.get('KT-player/shader/worldbubble')
     view = BGL.view.widescreen_16_9
     texture_idx = 0
     frt = 0
+    fidx = 0.0
     visible = False
 
     def reset():
@@ -14,6 +17,7 @@ class BubbleWorldRenderer():
         BubbleWorldRenderer.visible = True
 
     def tick():
+        BubbleWorldRenderer.fidx += 0.04
         BubbleWorldRenderer.frt += 1
         if(BubbleWorldRenderer.frt == 2):
             BubbleWorldRenderer.frt = 0
@@ -31,6 +35,18 @@ class BubbleWorldRenderer():
     def render():
 
         if not BubbleWorldRenderer.visible:
+            BubbleWorldRenderer.primitive.render_shaded( BubbleWorldRenderer.shader, {
+                "translation_local" : [ 0.0, 0.0 ],
+                "scale_local" : [ 1.0, 1,0 ],
+                "translation_world" : [ 0.0,0.0 ],
+                "scale_world" : [ 1.0,1.0 ],
+                "view" : BubbleWorldRenderer.view,
+                "rotation_local" : 0.0,
+                "filter_color" : [ 1.0,1.0,1.0,1.0],
+                "uv_translate" : [ 0.0,0.0],
+                "scale_x" : abs(sin(BubbleWorldRenderer.fidx)),
+                "texBuffer" : BubbleWorldRenderer.lock_texture } 
+            )
             return
 
         with BGL.blendmode.alpha_over:
@@ -43,6 +59,7 @@ class BubbleWorldRenderer():
                 "rotation_local" : 0.0,
                 "filter_color" : [ 1.0,1.0,1.0,1.0],
                 "uv_translate" : [ 0.0,0.0],
+                "scale_x" : 1.0,
                 "texBuffer" : BubbleWorldRenderer.textures[BubbleWorldRenderer.texture_idx] }
             )
 
